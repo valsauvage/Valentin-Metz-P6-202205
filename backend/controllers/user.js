20e41c57fc54c75b671a41ef6543a4f8259d2fb6
valsauvage <valsauvage@valsauvage.com>
2022-05-17 19:28:10 +0200

    réorganisation des dossiers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
